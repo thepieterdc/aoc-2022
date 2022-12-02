c25ed9519b5c367c0c545aee6bdb0418fb680fc4
Pieter De Clercq <pieter.de.clercq@deliverect.com>
2022-12-02 10:17:56 +0100

    Solve day 2

---
+++ day2/advanced.hs
@@ -0,0 +1,26 @@
+module Main where
+
+import           System.Environment (getArgs)
+
+import           Utils.Filtering    (isNumber)
+import           Utils.Parsing      (parseInt)
+
+score :: String -> Int
+score (op:_:m:_) = case (op,m) of
+    ('A', 'X') -> 3 + 0 -- Lose: Rock vs Scissors
+    ('A', 'Y') -> 1 + 3 -- Draw: Rock vs Rock
+    ('A', 'Z') -> 2 + 6 -- Win: Rock vs Paper
+    ('B', 'X') -> 1 + 0 -- Lose: Paper vs Rock
+    ('B', 'Y') -> 2 + 3 -- Draw: Paper vs Paper
+    ('B', 'Z') -> 3 + 6 -- Win: Paper vs Scissors
+    ('C', 'X') -> 2 + 0 -- Lose: Scissors vs Paper
+    ('C', 'Y') -> 3 + 3 -- Draw: Scissors vs Scissors
+    ('C', 'Z') -> 1 + 6 -- Win: Scissors vs Rock
+    _          -> 0
+score _ = 0
+
+main :: IO ()
+main = do
+    file:_ <- getArgs
+    contents <- readFile file
+    print $ sum (map score (lines contents))
---
+++ day2/simple.hs
@@ -0,0 +1,26 @@
+module Main where
+
+import           System.Environment (getArgs)
+
+import           Utils.Filtering    (isNumber)
+import           Utils.Parsing      (parseInt)
+
+score :: String -> Int
+score (op:_:m:_) = case (op,m) of
+    ('A', 'X') -> 1 + 3 -- Rock vs Rock
+    ('A', 'Y') -> 2 + 6 -- Rock vs Paper
+    ('A', 'Z') -> 3 + 0 -- Rock vs Scissors
+    ('B', 'X') -> 1 + 0 -- Paper vs Rock
+    ('B', 'Y') -> 2 + 3 -- Paper vs Paper
+    ('B', 'Z') -> 3 + 6 -- Paper vs Scissors
+    ('C', 'X') -> 1 + 6 -- Scissors vs Rock
+    ('C', 'Y') -> 2 + 0 -- Scissors vs Paper
+    ('C', 'Z') -> 3 + 3 -- Scissors vs Scissors
+    _          -> 0
+score _ = 0
+
+main :: IO ()
+main = do
+    file:_ <- getArgs
+    contents <- readFile file
+    print $ sum (map score (lines contents))
