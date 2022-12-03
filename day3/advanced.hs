4da0ecc71d6c1f8781201be7f8b4549c8ed2d3b5
Pieter De Clercq <pieter.de.clercq@deliverect.com>
2022-12-03 10:19:17 +0100

    Finish day 3

--- day3/advanced.hs
+++ day3/advanced.hs
@@ -2,17 +2,24 @@ module Main where
 
 import           System.Environment (getArgs)
 
+import           Data.List          (elemIndex, intersect)
+import           Data.Maybe         (fromJust)
 import           Utils.Filtering    (isNumber)
 import           Utils.Parsing      (parseInt)
 
-findMax :: Int -> Int -> [String] -> Int
-findMax m c [] = max m c
-findMax m c (x:xs) =   if isNumber x
-    then findMax m (c + parseInt x) xs
-    else findMax (max m c) 0 xs
+findBadges :: [String] -> [Char]
+findBadges [] = []
+findBadges lines = findBadge search:findBadges rest
+    where (search,rest) = splitAt 3 lines
+
+findBadge :: [String] -> Char
+findBadge [a,b,c] = head (a `intersect` b `intersect` c)
+
+prioritise :: Char -> Int
+prioritise c = fromJust (elemIndex c (['a'..'z'] ++ ['A'..'Z'])) + 1
 
 main :: IO ()
 main = do
     file:_ <- getArgs
     contents <- readFile file
-    print $ lines contents
+    print $ sum $ map prioritise (findBadges (lines contents))
