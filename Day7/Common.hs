27b8acbb1a5ba05c008b9f8d051d8cc45171894c
Pieter De Clercq <pieter.de.clercq@deliverect.com>
2022-12-08 23:20:19 +0100

    Cleanups

--- Day7/Common.hs
+++ Day7/Common.hs
@@ -1,16 +1,16 @@
 module Day7.Common (Tree(Dir, File), findDirectorySizes, run, size) where
 
-import           Data.Maybe   (catMaybes, fromJust, listToMaybe)
+import           Data.Maybe   (catMaybes)
 import           Prelude      hiding (until)
 import           Utils.Lists  (nothings)
-import           Utils.Parser (Parser, doParse, end, guard, integer, many, some,
+import           Utils.Parser (Parser, doParse, end, guard, integer, many,
                                string, token, until, void, (<|>))
 
 --
 -- Types.
 --
 
-data Tree = Dir String [Tree] | File String Int deriving (Show)
+data Tree = Dir [Tree] | File Int deriving (Show)
 
 --
 -- Parsing.
@@ -22,27 +22,27 @@ parse = parseDirectory
 parseDirectory :: Parser Tree
 parseDirectory = do
     string "$ cd "
-    name <- until '\n'
+    until '\n'
     string "$ ls\n"
     contents <- many parseDirectoryContents
     subDirectories <- many parseDirectory
     guard (nothings contents == length subDirectories)
     void (string "$ cd ..\n") <|> end
-    return $ Dir name (catMaybes contents ++ subDirectories)
+    return $ Dir (catMaybes contents ++ subDirectories)
 
 parseDirectoryContents :: Parser (Maybe Tree)
 parseDirectoryContents = parseFile <|> parseDirectoryName where
     parseDirectoryName = string "dir" >> until '\n' >> return Nothing
-    parseFile = do {size <- integer; token ' '; n <- until '\n'; return $ Just (File n size)}
+    parseFile = do {size <- integer; token ' '; until '\n'; return $ Just (File size)}
 
 findDirectorySizes :: Tree -> [Int]
-findDirectorySizes d@(Dir _ fs) = size d : concatMap findDirectorySizes fs
+findDirectorySizes d@(Dir fs) = size d : concatMap findDirectorySizes fs
 -- we only care about directories.
-findDirectorySizes _            = [0]
+findDirectorySizes _          = [0]
 
 run :: String -> Tree
 run = doParse parse
 
 size :: Tree -> Int
-size (Dir _ contents) = (sum . map size) contents
-size (File _ s)       = s
+size (Dir contents) = (sum . map size) contents
+size (File s)       = s
